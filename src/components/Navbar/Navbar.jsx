c2c443854a47e9de8bcc2cfce663edf300115d22
Htet-Myat-Linn <b8_htetmyatlinn@uit.edu.mm>
2022-03-15 20:34:51 +0630

    Project Structure and Responsive Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React,{ useState } from 'react';
+import { Link } from 'react-router-dom'
+import logo from '../../images/logo.png';
+import { HomeIcon,CurrencyPoundIcon,NewspaperIcon,ChartBarIcon,MenuAlt3Icon } from '@heroicons/react/solid';
+import './styles.css';
+
+
+const MenuItems = () => {
+  return (
+    <>
+      <div className='cryptoverse__container-links-container__link group'>
+        <HomeIcon className='cryptoverse__container-links-container__link__icon' />
+        <Link className='cryptoverse__container-links-container__link__text' to="/">Home</Link>
+      </div>
+      <div className='cryptoverse__container-links-container__link group'>
+        <CurrencyPoundIcon className='cryptoverse__container-links-container__link__icon' />
+        <Link className='cryptoverse__container-links-container__link__text' to="/cryptocurrencies">Cryptocurrencies</Link>
+      </div>
+      <div className='cryptoverse__container-links-container__link group'>
+        <ChartBarIcon className='cryptoverse__container-links-container__link__icon' />
+        <Link className='cryptoverse__container-links-container__link__text' to="/exchanges">Exchanges</Link>
+      </div>
+      <div className='cryptoverse__container-links-container__link group'>
+        <NewspaperIcon className='cryptoverse__container-links-container__link__icon' />
+        <Link className='cryptoverse__container-links-container__link__text' to="/news">News</Link>
+      </div>
+    </>
+  );
+}
+
+const Navbar = () => {
+  const [toggleMenu,setToggleMenu] = useState(false);
+  return (
+    <div className='cryptoverse__container'>
+        <div className='cryptoverse__container-brand'>
+            <img src={logo} alt="Cryptoverse Logo" />
+            <h1 className='gradient-text'>Cryptoverse</h1>
+        </div>
+        <div className='cryptoverse__container-links-container'>
+          <MenuItems />
+        </div>
+        <div className='cryptoverse__container-menu'>
+          <button className='cryptoverse__container-menu-button' onClick={() => setToggleMenu(!toggleMenu)}>
+            <MenuAlt3Icon className='cryptoverse__container-menu-button-icon' />
+          </button>
+          {
+            toggleMenu && (
+              <div className='cryptoverse__container-mobile-links-container'>
+                  <MenuItems />
+              </div>
+            )
+          }
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'main' : "#040c18",
+      'text-blue' : "#3860FF",
+      'text-red' : "#FF0800",
+      'transparent' : "transparent",
+      'white' : "#fff"
+    },
+    fontFamily: {
+      'main' : ['IBM Plex Mono','monospace'],
+    },
+  },
+  plugins: [],
+}
